6324d6028e9c4df1c57278fae51c64b647f7a973
Mike Taylor <mike@indexdata.com>
2024-09-27 10:54:58 +0100

    New

---
+++ c3.sql
@@ -0,0 +1,21 @@
+--metadb:function checkins_by_date
+
+DROP FUNCTION IF EXISTS checkins_by_date;
+
+CREATE FUNCTION checkins_by_date(
+    start_date date DEFAULT '2024-01-01',
+    end_date date DEFAULT '9999-12-31')
+RETURNS TABLE(
+    checkin_date date,
+    count integer
+)
+AS $$
+SELECT date(occurred_date_time) as checkin_date, count(*)
+	FROM folio_circulation.check_in__t
+	WHERE start_date <= occurred_date_time::date AND occurred_date_time::date < end_date
+	GROUP BY date(occurred_date_time)
+	ORDER BY date(occurred_date_time)
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
